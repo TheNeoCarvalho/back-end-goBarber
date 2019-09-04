5f0d41e02ad5130cf844a2edd73076b8cbb9ba06
Manoel Carvalho <manoelmecat@gmail.com>
2019-09-04 20:40:27 -0300

    Controller store

--- src/database/migrations/20190904022156-create-users.js
+++ src/database/migrations/20190904022156-create-users.js
@@ -25,11 +25,11 @@ module.exports = {
         defaultValue: false,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({
-    message: 'Opa, massa',
-  });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
