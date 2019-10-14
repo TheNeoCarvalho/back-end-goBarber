804e5a20324d877b81aa5d2bd0569a0abbaa300f
Manoel Carvalho <manoelmecat@gmail.com>
2019-10-13 22:40:40 -0300

    Queue

--- src/libs/Queue.js
+++ src/libs/Queue.js
@@ -30,8 +30,12 @@ class Queue {
   processQueue() {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
-      bee.process(handle);
+      bee.on('failed', this.handleFailed).process(handle);
     });
+  }
+
+  handleFailed(job, err) {
+    console.log(`Queue ${job.queue.name}>FAILED`, err);
   }
 }
 
